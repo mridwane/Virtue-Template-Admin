e847d5230f06615e74e60ebf683f5a28e3700596
mridwane <mridwane01@gmail.com>
2021-09-02 20:32:13 +0700

    Menambahkan Modal Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,5 +5,37 @@ $(document).ready(function(){
     $("#btn-sidebar").on("click",function(){
         $("#sidebar").removeClass("hidden");
     });
+
+    // $("#close-simple-modal").on("click",function(){
+    //     $("#simpleModal").addClass("hidden");
+    // });
+
+    // $("#simple-modal").on("click",function(){
+    //     $("#simpleModal").removeClass("hidden");
+    // });
+
+    $("[data-dismiss='modal']").click(function() {
+        $("[data-modal='simpleModal']").addClass("hidden");
+    });
+
+    $("[data-modalButton='simple-modal']").click(function() {
+        $("[data-modal='simpleModal']").removeClass("hidden");
+    });
+
+    $("[data-dismiss='modal']").click(function() {
+        $("[data-modal='defaultModal']").addClass("hidden");
+    });
+
+    $("[data-modalButton='default-modal']").click(function() {
+        $("[data-modal='defaultModal']").removeClass("hidden");
+    });
+
+    $("[data-dismiss='modal']").click(function() {
+        $("[data-modal='lineModal']").addClass("hidden");
+    });
+
+    $("[data-modalButton='line-modal']").click(function() {
+        $("[data-modal='lineModal']").removeClass("hidden");
+    });
 });
 
